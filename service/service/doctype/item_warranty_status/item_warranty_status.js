517de70a154ae9e32084b21f2c52f54a9bec9926
Abaikarthik <abai.karthik@aalamsoft.com>
2024-12-13 05:00:14 +0000

    Initial Commit

---
+++ service/service/doctype/item_warranty_status/item_warranty_status.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Aalamsoft and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Item Warranty Status", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ service/service/doctype/item_warranty_status/test_item_warranty_status.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Aalamsoft and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestItemWarrantyStatus(FrappeTestCase):
+	pass
